cf0ffee53134c9ddf3bad5eb358ba68dcf29810d
arxfv <dveheheyeggsbejnxowbrirbbnrihb@gmail.com>
2026-08-13 17:25:01 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
+// ترويسات الحماية والأمان (Security Headers)
+app.use((req, res, next) => {
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    next();
+});
+
 const dbPath = path.join(__dirname, 'meswak_clinic_db.json');
 
 // قراءة قاعدة البيانات
